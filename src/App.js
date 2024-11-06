0b47878646f44288f7f6bfa11d768233c1077983
rohnshrma <rohn11shrma@gmail.com>
2024-11-06 23:57:07 +0530

    transporting data from createArea to app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-// Importing the Footer component from the Components folder
+// Importing the Counter component from the Components folder
 import Counter from "./Components/Counter";
+
+// Importing the CreateArea component from the Components folder
 import CreateArea from "./Components/CreateArea";
+
+// Importing the Footer component from the Components folder
 import Footer from "./Components/Footer";
 
 // Importing the Header component from the Components folder
 import Header from "./Components/Header";
+
+// Importing the ShowTime component from the Components folder
 import ShowTime from "./Components/ShowTime";
 
 // Importing the Tasks component from the Components folder
@@ -15,23 +21,33 @@ import tasksData from "./data";
 
 // Defining the main App component as an arrow function
 const App = () => {
-  // Returning JSX (a syntax extension for writing HTML within JavaScript)
+  // Defining addTaskHandler function to handle adding a new task
+  // The function accepts a parameter 'taskObj' which represents the new task object
+  const addTaskHandler = (taskObj) => {
+    // Logging the incoming task data to the console for debugging or monitoring
+    console.log("Incoming Data =>", taskObj);
+  };
+
+  // Returning JSX (JavaScript XML), a syntax for writing HTML in JavaScript, to define the UI of the App component
   return (
-    // Main container div for the App component, with a class name of "app"
+    // Main container div for the App component, with a class name of "app" for styling purposes
     <div className="app">
-      {/* Rendering the Header component */}
+      {/* Rendering the Header component at the top of the App */}
       <Header />
 
-      {/* Rendering the Tasks component and passing the tasksData as a prop */}
+      {/* Rendering the Tasks component and passing the tasksData array as a prop */}
+      {/* The Tasks component will use tasksData to display each task */}
       {/* <Tasks tasks={tasksData} /> */}
 
-      <CreateArea />
+      {/* Rendering the CreateArea component and passing addTaskHandler as a prop called 'onAdd' */}
+      {/* This will allow CreateArea to trigger addTaskHandler when a new task is created */}
+      <CreateArea onAdd={addTaskHandler} />
 
-      {/* Rendering the Footer component */}
+      {/* Rendering the Footer component at the bottom of the App */}
       <Footer />
     </div>
   );
 };
 
-// Exporting the App component so it can be imported and used in other files
+// Exporting the App component to make it available for import in other files
 export default App;
--- src/Components/CreateArea.js
+++ src/Components/CreateArea.js
@@ -1,50 +1,85 @@
+// Importing the useState function from React, which helps us manage and update component data (state)
 import { useState } from "react";
 
-const CreateArea = () => {
+// Defining the CreateArea component as a function
+// This component will allow the user to input and submit a new task
+const CreateArea = (props) => {
+  // Defining a "name" state variable to store the task's name
+  // "setName" is a function that allows us to update "name"
+  // Initially, "name" is set to an empty string ("")
   const [name, setName] = useState("");
+
+  // Defining a "desc" (description) state variable to store the task's description
+  // "setDesc" allows us to update "desc"
+  // Initially, "desc" is also set to an empty string ("")
   const [desc, setDesc] = useState("");
 
+  // Defining a function to handle changes to the task name input
+  // This function will be called whenever the user types in the name input box
   const nameChangeHandler = (e) => {
+    // Logging the current value of the input to the console for debugging
     console.log(e.target.value);
+
+    // Updating the "name" state with the current value of the input
     setName(e.target.value);
   };
 
+  // Defining a function to handle changes to the task description input
+  // This function will be called whenever the user types in the description input box
   const descChangeHandler = (e) => {
+    // Logging the current value of the input to the console for debugging
     console.log(e.target.value);
+
+    // Updating the "desc" state with the current value of the input
     setDesc(e.target.value);
   };
 
+  // Defining a function to handle the form submission (when the user clicks "Add Task")
   const submitHandler = (e) => {
+    // Preventing the page from refreshing when the form is submitted
     e.preventDefault();
 
-    // make an object using both name & desc
+    // Creating an object that combines the task's name and description
     const taskObj = {
-      name,
-      desc,
+      name, // task name
+      desc, // task description
     };
 
+    // Logging the task object to the console for debugging
     console.log(taskObj);
+
+    // Passing the new task object to a function received through props (from the parent component)
+    // This will allow the parent component to use the new task data
+    props.onAdd(taskObj);
   };
 
+  // Returning the form layout (JSX) that will be displayed on the page
   return (
+    // Form element with a class name "createarea" for styling, and an onSubmit event for handling submission
     <form className="createarea" onSubmit={submitHandler}>
+      {/* First input field for the task name */}
       <div className="form-group">
         <input
-          onChange={nameChangeHandler}
-          type="text"
-          placeholder="Task Name..."
+          onChange={nameChangeHandler} // This calls nameChangeHandler whenever the input changes
+          type="text" // Input type is text
+          placeholder="Task Name..." // Placeholder text for the input field
         />
       </div>
+
+      {/* Second input field for the task description */}
       <div className="form-group">
         <input
-          onChange={descChangeHandler}
-          type="text"
-          placeholder="Task Description..."
+          onChange={descChangeHandler} // This calls descChangeHandler whenever the input changes
+          type="text" // Input type is text
+          placeholder="Task Description..." // Placeholder text for the input field
         />
       </div>
+
+      {/* Button to submit the form and add the new task */}
       <button>Add Task</button>
     </form>
   );
 };
 
+// Exporting the CreateArea component so it can be used in other parts of the app
 export default CreateArea;
